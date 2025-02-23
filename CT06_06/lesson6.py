2ad25902d954f9f3192a07b56788c9cf1b19513d
Kaius-Pythonthinker1 <kaiusgoh8888@gmail.com>
2025-02-23 15:56:21 +0000

    Feb 23, 2025, 3:56 PM

--- CT06_06/lesson6.py
+++ CT06_06/lesson6.py
@@ -1,1 +1,9 @@
-print("Hello from lesson 6")
+NumStud = int(input("How many students are there?"))
+
+SumScore = 0
+
+for i in range(NumStud):
+    score = int(input("What did this student get?"))
+    SumScore = SumScore + score
+
+print("The average score of all the students is " + str(SumScore/NumStud))
